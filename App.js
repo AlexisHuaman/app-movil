22e4255ed18a1df3eaac6b5435b62a36a2ed4962
Alexis <ahuamani@unjbg.edu.pe>
2024-09-26 11:06:41 -0500

    version 1

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native"; // Asegúrate de importar Image
+
+import logo from "./assets/logo.jpg"; // Importar la imagen
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+      <Text>Dame plata</Text>
+
+      <TouchableOpacity
+        onPress={() => alert("-100 soles")}
+        style={{
+          width: 120,
+          height: 20,
+          backgroundColor: "red",
+          borderRadius: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white" }}>Regalar dinero</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +37,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 100, // Especifica el ancho de la imagen
+    height: 100, // Especifica la altura de la imagen
+    marginBottom: 20, // Agrega un espacio debajo de la imagen
   },
 });
